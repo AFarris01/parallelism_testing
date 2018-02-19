d649c99f2bad197210a4fb79627f0237f20f9eba
Andrew Farris <afarris@groupcbs.com>
2018-02-19 16:28:20 -0600

    Consolidated the fetcher summaries to reduce redundancy. Consolidated the fetcher summaries to reduce redundancy. Consolidated the fetcher summaries to reduce redundancy.
    Signed-off-by: Andrew Farris <afarris01@gmail.com>

--- thread_test.py
+++ thread_test.py
@@ -44,53 +44,46 @@ def fetchURL(url):
 def sequenceFetch(urls,details=False):
     """ Fetch a given list of URLs serially, and print some statistics on how long it took
     """
-    startime = time()
     if details:
         fetcher=fetchURL
     else:
         fetcher=fetchURLm
-    allresults = [fetcher(url) for url in urls]
-    totalfetched = sum([len(r.content) for r in allresults if r])
-    num=len(urls)
-    duration = time()-startime
-    print(f"\tRetrieved a total of {totalfetched} bytes from {num} URLs in {duration:.4f}s")
+    return [fetcher(url) for url in urls]
 
-def asyncFetch(urls):
+def asyncFetch(urls,details=False):
     """ Fetch a given list of URLs with a thread pool, and print some statistics on how long it took
     """
-    startime = time()
     allresults = []
     with cf.ThreadPoolExecutor() as executor:
         allresults = executor.map(fetchURLm,urls)
+    return allresults
 
-    totalfetched = sum([len(r.content) for r in allresults if r])
-    num=len(urls)
-    duration = time()-startime
-    print(f"\tRetrieved a total of {totalfetched} bytes from {num} URLs in {duration:.4f}s")
-
-def parallelFetch(urls):
+def parallelFetch(urls,details=False):
     """ Fetch a given list of URLs with a process pool, and print some statistics on how long it took
     """
-    startime = time()
     allresults = []
     with mp.Pool() as pool:
         allresults = pool.map(fetchURLm,urls)
-
+    return allresults
+    
+def fetchSummarize(urls, fetcher=sequenceFetch, details=False):
+    startime = time()
+    allresults = fetcher(urls,details)
     totalfetched = sum([len(r.content) for r in allresults if r])
     num=len(urls)
     duration = time()-startime
     print(f"\tRetrieved a total of {totalfetched} bytes from {num} URLs in {duration:.4f}s")
-                        
 
+    
 if __name__ == '__main__':
     print("Details on proposed sequence:")
-    sequenceFetch(urlist,True)
+    fetchSummarize(urlist,details=True)
     
     print("Timing sequential URL requests...")
-    sequenceFetch(urlist)
+    fetchSummarize(urlist)
 
     print("Timing Async URL requests...")
-    asyncFetch(urlist)
+    fetchSummarize(urlist,asyncFetch)
 
     print("Timing Parallel URL requests...")
-    parallelFetch(urlist)
+    fetchSummarize(urlist,parallelFetch)
